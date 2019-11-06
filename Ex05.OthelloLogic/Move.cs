a863ce660713affef997869ed4eed4519bdff850
eli185 <48991084+eli185@users.noreply.github.com>
2019-11-06 09:34:22 +0200

    Add files via upload

---
+++ Ex05.OthelloLogic/Move.cs
@@ -0,0 +1,40 @@
+﻿namespace Ex05.OthelloLogic
+{
+    public class Move
+    {
+        private Square m_Source;
+        private Square m_Destination;
+        private MoveDirectionEnum.eMoveDirection m_Direction;
+
+        public Square From
+        {
+            get
+            {
+                return m_Source;
+            }
+        }
+
+        public Square To
+        {
+            get
+            {
+                return m_Destination;
+            }
+        }
+
+        public MoveDirectionEnum.eMoveDirection Direction
+        {
+            get
+            {
+                return m_Direction;
+            }
+        }
+
+        public Move(Square i_From, Square i_To, MoveDirectionEnum.eMoveDirection i_Direction)
+        {
+            m_Source = i_From;
+            m_Destination = i_To;
+            m_Direction = i_Direction;
+        }
+    }
+}
---
+++ Ex05.OthelloLogic/Square.cs
@@ -0,0 +1,41 @@
+﻿namespace Ex05.OthelloLogic
+{
+    public class Square
+    {
+        private int m_Row;
+        private int m_Column;
+
+        public static bool AreEqualSquares(Square i_Square1, Square i_Square2)
+        {
+            return (i_Square1.Row == i_Square2.Row) && (i_Square1.Column == i_Square2.Column);
+        }
+
+        public int Row
+        {
+            get
+            {
+                return m_Row;
+            }
+        }
+
+        public int Column
+        {
+            get
+            {
+                return m_Column;
+            }
+        }
+
+        public Square(int i_Row, int i_Column)
+        {
+            m_Row = i_Row;
+            m_Column = i_Column;
+        }
+
+        public Square(int i_Row, char i_Column)
+        {
+            m_Row = i_Row;
+            m_Column = i_Column - 'A' + 1;
+        }
+    }
+}
